4e3d07a19628a1b68b1addf05c7c0a0ce5010cbe
deep6000 <deso6761@colorado.edu>
2019-07-21 01:16:01 -0600

    Hough Line : Adding Output images

--- Assignment4/Ques2/hough_line.cpp
+++ Assignment4/Ques2/hough_line.cpp
@@ -1,17 +1,26 @@
+/**
+ * @file hough_line.cpp
+ * @author Deepesh 
+ * @brief Draw Hough lines (Probabilistic and non probabilistic)
+ * @version 0.1
+ * @date 2019-07-17
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
-
+#include "opencv2/imgcodecs/imgcodecs.hpp"
 #include <iostream>
 
 using namespace cv;
 using namespace std;
 
-void help()
-{
- cout << "\nThis program demonstrates line finding with the Hough transform.\n"
-         "Usage:\n"
-         "./houghlines <image_name>, Default is pic1.jpg\n" << endl;
-}
+/**
+ * @brief helper function
+ * 
+ */
+void help();
 
 int main(int argc, char** argv)
 {
@@ -25,13 +34,13 @@ int main(int argc, char** argv)
      return -1;
  }
 
- Mat dst, cdst;
+ Mat dst, cdst, cdstP;
  Canny(src, dst, 50, 200, 3);
  cvtColor(dst, cdst, CV_GRAY2BGR);
 
- #if 0
-  vector<Vec2f> lines;
-  HoughLines(dst, lines, 1, CV_PI/180, 100, 0, 0 );
+ cdstP = cdst.clone();
+ vector<Vec2f> lines;
+ HoughLines(dst, lines, 1, CV_PI/180, 150, 0, 0 );
 
   for( size_t i = 0; i < lines.size(); i++ )
   {
@@ -43,21 +52,30 @@ int main(int argc, char** argv)
      pt1.y = cvRound(y0 + 1000*(a));
      pt2.x = cvRound(x0 - 1000*(-b));
      pt2.y = cvRound(y0 - 1000*(a));
-     line( cdst, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
+     line( cdst, pt1, pt2, Scalar(0,0,255), 3, LINE_AA);
   }
- #else
-  vector<Vec4i> lines;
-  HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10 );
-  for( size_t i = 0; i < lines.size(); i++ )
-  {
-    Vec4i l = lines[i];
-    line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
-  }
- #endif
- imshow("source", src);
- imshow("detected lines", cdst);
 
+  vector<Vec4i> linesP;
+  HoughLinesP(dst, linesP, 1, CV_PI/180, 50, 50, 10 );
+  for( size_t i = 0; i < linesP.size(); i++ )
+  {
+    Vec4i l = linesP[i];
+    line( cdstP, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+  }
+ imshow("source", src);
+ imshow("detected lines Standard Hough transform", cdst);
+ imshow("detected lines Probabilistic Hough transform", cdstP);
+ imwrite("Standard Hough transform.jpg", cdst);
+ imwrite("Probabilistic Hough transform.jpg", cdstP);
  waitKey();
 
  return 0;
+}
+
+
+void help()
+{
+ cout << "\nThis program demonstrates line finding with the Hough transform.\n"
+         "Usage:\n"
+         "./houghlines <image_name>" << endl;
 }
